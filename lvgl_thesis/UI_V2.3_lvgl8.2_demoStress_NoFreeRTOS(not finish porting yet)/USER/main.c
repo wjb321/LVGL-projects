7287d9ce7633130c867250e9de94a042cb05f5b7
wjb321 <44176516+wjb321@users.noreply.github.com>
2023-02-21 08:29:28 +0800

    Update main.c
    Delete some useless freertos lines cause this one is an intermediate code, where freertos driver files are inside but not called; here, only the lvgl runs.

--- lvgl_thesis/UI_V2.3_lvgl8.2_demoStress_NoFreeRTOS(not finish porting yet)/USER/main.c
+++ lvgl_thesis/UI_V2.3_lvgl8.2_demoStress_NoFreeRTOS(not finish porting yet)/USER/main.c
@@ -14,34 +14,6 @@
 #include "lv_port_indev.h"
 #include "lv_demo_keypad_encoder.h"
 #include "lv_demo_stress.h"
-//#include "FreeRTOS.h"
-//#include "task.h"
-//#include "lv_demo_music.h"
-//#include "lv_apps\demo\demo.h"
-//#include "lv_tests\lv_test_theme\lv_test_theme_1.h"
-//#include "lv_tests\lv_test_theme\lv_test_theme_2.h"
-//#include "gui_app.h"
-//#include "chart_win.h"
-//#include "lv_port_disp.h"
-
-
-
-//#define START_TASK_PRIO		1
-//#define START_STK_SIZE 		128  
-//TaskHandle_t StartTask_Handler;
-//void start_task(void *pvParameters);
-
-
-//#define LVGL_TASK_PRIO		2
-//#define LVGL_STK_SIZE 		128  
-//TaskHandle_t LVGL_Task_Handler;
-//void LVGL_task(void *pvParameters);
-
-
-//#define TASK2_TASK_PRIO		3
-//#define TASK2_STK_SIZE 		128  
-//TaskHandle_t Task2Task_Handler;
-//void task2_task(void *pvParameters);
 
 int arrValue = 199;
 int pscValue = 71;
@@ -63,173 +35,29 @@ extern int updateChart;
 
 int main(void)
 {
-  delay_init();	    	 		//延时函数初始化
-  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
-  uart_init(115200);	 		//串口初始化为115200
-  LED_Init();			     		//LED端口初始化
-  KEY_Init();	 						//按键初始化
-  BEEP_Init();						//蜂鸣器初始化
-  FSMC_SRAM_Init();				//外部1MB的sram初始化
-	CAN_ABSCONTROL_Configuration();
+  delay_init();	    	 		
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+  uart_init(115200);	 		
+  LED_Init();			     		
+  KEY_Init();	 					
+  BEEP_Init();					
+  FSMC_SRAM_Init();				
+  CAN_ABSCONTROL_Configuration();
   TIM1_Int_Init(99, 7199, TIM1_Enable);
   TIM2_Int_Init(arrValue,pscValue,ENABLE);
-  TIM3_PWM_Init(1399,0);	 //不分频。PWM频率=72000000/900=80Khz
-  TIM5_Int_Init(999,71);	//定时器初始化(1ms中断),用于给lvgl提供1ms的心跳节拍
+  TIM3_PWM_Init(1399,0);	
+  TIM5_Int_Init(999,71);	
   TIM6_Int_Init(9999,7199, ENABLE);
   TIM4_EncoderMode_Config(Pulse);
 
-  //LCD_Init();							//LCD初始化
-  lv_init();							//lvgl系统初始化
-	lv_port_disp_init();
-  lv_port_indev_init();		//lvgl输入接口初始化
-	 lv_demo_stress();
-	while(1)
-	{
-				lv_timer_handler(); /* LVGL计时器 */
-       delay_ms(5);
-	}
-	
-	// tp_dev.init();		
-//      xTaskCreate((TaskFunction_t )start_task,            //任务函数
-//                (const char*    )"start_task",          //任务名称
-//                (uint16_t       )START_STK_SIZE,        //任务堆栈大小
-//                (void*          )NULL,                  //传递给任务函数的参数
-//                (UBaseType_t    )START_TASK_PRIO,       //任务优先级
-//                (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
-//    vTaskStartScheduler();          //开启任务调度
-}
-
-//void start_task(void *pvParameters)
-//{  
-//	 pvParameters = pvParameters;
-//    taskENTER_CRITICAL();           //进入临界区
-//    //创建TASK1任务
-//    xTaskCreate((TaskFunction_t )LVGL_task,             
-//                (const char*    )"LVGL_task",           
-//                (uint16_t       )LVGL_STK_SIZE,        
-//                (void*          )NULL,                  
-//                (UBaseType_t    )LVGL_TASK_PRIO,        
-//                (TaskHandle_t*  )&LVGL_Task_Handler);   
-//    //创建TASK2任务
-//    xTaskCreate((TaskFunction_t )task2_task,     
-//                (const char*    )"task2_task",   
-//                (uint16_t       )TASK2_STK_SIZE,
-//                (void*          )NULL,
-//                (UBaseType_t    )TASK2_TASK_PRIO,
-//                (TaskHandle_t*  )&Task2Task_Handler); 
-//    taskEXIT_CRITICAL();            //退出临界区
-//	  vTaskDelete(StartTask_Handler); //删除开始任务
-//}
-
-
-//task2任务函数
-//void task2_task(void *pvParameters)
-//{
-//	while(1)
-//	{
-//	       LED1=!LED1;
-//        vTaskDelay(1000);                          
-//	}
-//}
-
-//task1任务函数
-void LVGL_task(void *pvParameters)
-{  vu8 key=0;
-//	pvParameters = pvParameters;
-//	  lv_demo_stress();
-//	
+  //LCD_Init();					
+  lv_init();					
+  lv_port_disp_init();
+  lv_port_indev_init();		
+  lv_demo_stress();
   while(1)
-    { 
-//			   lv_timer_handler(); /* LVGL计时器 */
-//        vTaskDelay(5);
-//      tp_dev.scan(0);
-//      lv_task_handler();
-//      key=KEY_Scan(0);	//得到键值
-//      if(key)
-//        {
-//          switch(key)
-//            {
-//            case KEY2_PRES:  //change the current trend:celeration-> deceleration or vice versa
-//              CeleOrDeceFlag = !CeleOrDeceFlag;
-//              printf("CeleOrDeceFlag is %d\n",CeleOrDeceFlag);
-//              break;
-
-//            case KEY1_PRES:  // initial the speed either highest or lowest
-//              InitPWMFlag =!InitPWMFlag;
-//              if(InitPWMFlag) led0pwmval = 30;
-//              else led0pwmval = 1079;
-//              printf("led0pwmval is %d\n",led0pwmval);
-//              break;
-
-//            case KEY0_PRES:  // deceleration or celeration rate change
-//              PWMChangeFlag =!PWMChangeFlag;
-//              if(PWMChangeFlag) SpeedDecrease = 6;
-//              else SpeedDecrease = 3;
-//              printf("SpeedDecrease is %d\n",SpeedDecrease);
-//              break;
-
-//            case WKUP_PRES:  // keep current speed or keep the previous trend or or off timer
-//              ConstantOrChangeSpeed = !ConstantOrChangeSpeed;
-//              TIM_Cmd(TIM1, ConstantOrChangeSpeed);
-//              printf("ConstantOrChangeSpeed is %d\n",ConstantOrChangeSpeed);
-//              break;
-//            }
-//        }
-//      switch(CAN_Speedflag)
-//        {
-//        case 1:
-//          if(led0pwmval <= 30) CeleOrDeceFlag = 0;
-//          else if(led0pwmval >= 1079) CeleOrDeceFlag = 1;
-//          switch(CeleOrDeceFlag)
-//            {
-//            case 0:
-//              led0pwmval +=SpeedDecrease; // decelation
-//						  //BEEP = !BEEP;
-//						  //printf("led0pwmval is %d\n",led0pwmval);
-//              break;
-//            case 1:
-//              led0pwmval -=SpeedDecrease; // celelation
-//              break;
-//            default:
-//              break;
-//            }
-//          CAN_Speedflag = 0;
-//          break;
-//        case 2:
-//					 switch(Rx1_DATA0)
-//					 {
-//						 case 1:
-//							 SpeedDecrease = 6;
-//						   led0pwmval -= SpeedDecrease;
-//						   BEEP = !BEEP;
-//							 //printf("blocked\n");
-//							 break;
-//						 case 2:
-//							 printf("celeration\n");
-//							 break;
-//						 case 3:
-//							 printf("celeration\n");
-//							 break;
-//						 case 4:
-//							 printf("constant\n");
-//							 break;
-//						 default:
-//							 break;
-//					 }
-//					 CAN_Speedflag = 0;
-//          break;
-//        default:
-//          break;
-//        }
-//      TIM_SetCompare2(TIM3,led0pwmval);
-//		//		printf("hello the world'\r\n");
-//					//delay_ms(500);
-//		//lv_timer_handler();
-//  		   LED0=!LED0;
-//        vTaskDelay(1000);
-   }
-
+   {
+     lv_timer_handler();
+     delay_ms(5);
+    }
 }
-
-
-
